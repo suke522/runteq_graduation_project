1f5818382a557dd1716596f75e53cba47592d35e
suke522 <ryouauke5221522@gmail.com>
2025-10-08 22:00:57 +0900

    記録作成機能の実装

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,24 @@ class WorksController < ApplicationController
   def index
     @works = Work.includes(:user)
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = current_user.works.build(work_params)
+    if @work.save
+      redirect_to works_path, success: t("defaults.flash_message.created", item: Work.model_name.human)
+    else
+      flash.now[:danger] = t("defaults.flash_message.not_created", item: Work.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :memo)
+  end
 end
